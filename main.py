3d209dd98c4b578e99224ce7fcb4dfb799e381c0
Yrjan Rein <yrjan4@gmail.com>
2025-04-24 17:56:45 +0200

    Added a counter for words in file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+
+def get_book_text(path):
+    with open(path) as file:
+        file_contents = file.read()
+        return file_contents
+
+def word_count(string):
+    num_words = len((string.split()))
+    return num_words
+
+def main():
+    num_words = word_count(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
+
+main()
